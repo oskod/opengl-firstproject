d7b60d7c8b9da3710716945ed807924d704f3145
dokso <ulkonmogo@gmail.com>
2023-05-11 23:46:05 +0200

    hello triangle, not finished

--- src/main.c
+++ src/main.c
@@ -8,6 +8,21 @@
 void __framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
+
+const char* vertexShaderSource = 
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main() {\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.Z, 1.0f);\n"
+"}\0";
+const char* fragmentShaderSource = 
+"#version 330 core\n"
+"out vec4 fragColor\n"
+"void main() {\n"
+"	fragColor = vec4(0.5f, 1.0f, 0.2f, 1.0f);\n"
+"}\0";
+
+
 int main() {
 #ifdef DEBUG
 	printf("Hello, OpenGL!\n");
@@ -47,13 +62,80 @@ int main() {
 	}
 	glViewport(0, 0, 800, 600);
 
+
+	// create first triangle
+	
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	// register an id for the triangle
+	unsigned int TBO;
+	glGenBuffers(1, &TBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, TBO); // to opengl: hey, this is the buffer you need to write to when writing data to GL_ARRAY_BUFFER
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+
+	// shaders
+	int success;
+	char infoLog[512];
+
+	// compile vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("OPENGL VERTEX SHADER COMPILATION FAILED\n%s", infoLog);
+	}
+
+	// compile fragment shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		printf("OPENGL FRAGMENT SHADER COMPILATION FAILED\n%s", infoLog);
+	}
+
+	// create shader program and attach shaders
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
+		printf("OPENGL SHADER PROGRAM LINKING FAILED\n%s", infoLog);
+	}
+
+	glUseProgram(shaderProgram);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//TODO continue from "Linking Vertex Attributes" - https://learnopengl.com/Getting-started/Hello-Triangle
+
+
 	// render loop
 	while (!glfwWindowShouldClose(window)) {
 		// input
 		processInput(window);
 
-		glClearColor(0.3f, 0.2f, 0.2f, 1.0f);
+		glClearColor(0.5f, 0.2f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
 
 		// render instructions are finished, swap buffers and render to the screen
 		glfwSwapBuffers(window);
