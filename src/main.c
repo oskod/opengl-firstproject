fde76077afbde3a1a0b7102a7614a870f3504cd1
oskod <ulkonmogo@gmail.com>
2023-05-12 13:46:35 +0200

    hello triangle finished

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 #include "GLAD/glad.h"
 #include "GLFW/glfw3.h"
 
+const int width = 800;
+const int height = 600;
+
 void __framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -19,7 +22,7 @@ const char* fragmentShaderSource =
 	"#version 330 core\n"
 	"out vec4 fragColor;\n"
 	"void main() {\n"
-	"	fragColor = vec4(0.5f, 1.0f, 0.2f, 1.0f);\n"
+	"	fragColor = vec4(0.5f, 0.2f, 1.0f, 1.0f);\n"
 	"}";
 
 
@@ -39,7 +42,7 @@ int main() {
 #endif
 
 	// glfw window creation
-	GLFWwindow* window = glfwCreateWindow(800, 600, "my first opengl project!!!!!!", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "my first opengl project!!!!!!", NULL, NULL);
 	if (window == NULL) {
 		#ifdef DEBUG
 		printf("Failed to create window\n");
@@ -60,7 +63,7 @@ int main() {
 		glfwTerminate();
 		return -1;
 	}
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, width, height);
 
 
 	// shaders
@@ -115,44 +118,73 @@ int main() {
 		return -1;
 	}
 
-	glUseProgram(shaderProgram);
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
-
-	//TODO continue from "Linking Vertex Attributes" - https://learnopengl.com/Getting-started/Hello-Triangle
 
 
 	// create first triangle
 	
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		0.5f, 0.5f, 0.0f, // top right
+		0.5f, -0.5f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f, // bottom left
+		-0.5f, 0.5f, 0.0f // top left
+	};
+	unsigned int faces[] = {
+		0, 1, 3,
+		1, 2, 3
 	};
 
-	// register an id for the triangle
-	unsigned int TBO;
-	glGenBuffers(1, &TBO);
 
-	glBindBuffer(GL_ARRAY_BUFFER, TBO); // to opengl: hey, this is the buffer you need to write to when writing data to GL_ARRAY_BUFFER
+	// buffers
+	unsigned int VAO, VBO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	// bind vertex array, so all buffers get stored in it
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // to opengl: hey, this is the buffer you need to write to when writing data to GL_ARRAY_BUFFER
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// doesn't sizeof(vertices) give the size of the pointer and not the array??
-	// i'll have to research later
+	// doesn't sizeof(vertices) give the size of the pointer and not the array? I'll have to do some research later.
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(faces), faces, GL_STATIC_DRAW);
 
+	// tell opengl how to read the data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
+	// unbind vertex array before the buffers, to ensure the bound buffers are stored in it
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	// render loop
 	while (!glfwWindowShouldClose(window)) {
 		// input
 		processInput(window);
 
-		glClearColor(0.5f, 0.2f, 0.2f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.11f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		// render instructions are finished, swap buffers and render to the screen
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	// kill everything
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
 	return 0;
